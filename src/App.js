ea183ad8d6f27f6a480ce05f703f0c1e2b19bc59
ALWAYSETHIOPIA <157984328+ALWAYSETHIOPIA@users.noreply.github.com>
2024-07-11 23:15:36 -0400

    last

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-
-import './App.css';
-import Home from './Pages/Home/Home';
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import { HashRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-   <Home />
-    </div>
+    <HashRouter>
+      <div className="App">
+        <Home />
+      </div>
+    </HashRouter>
   );
 }
 
